9d70609593c1f8afae469d1d3680a7a7c99f087a
NexyusNex <shootdietale@gmail.com>
2023-01-11 18:18:49 +0100

    Added a way to add both X and O to the table

--- javascript.js
+++ javascript.js
@@ -1,8 +1,24 @@
+var turn = true;
+
 const gameBoard = (() => {
-  let board = ["x", "o", "o", "x", "o", "x", "o", "o", "x"];
+  let board = ["", "", "", "", "", "", "", "", ""];
+
+  function UseMove(index) {
+    if (board[index] == "") {
+      if (turn == true) {
+        board[index] = "X";
+        turn = false;
+      } else {
+        board[index] = "O";
+        turn = true;
+      }
+      Display();
+    }
+  }
 
   return {
     board,
+    UseMove: UseMove,
   };
 })();
 
@@ -10,16 +26,21 @@ const Player = (player) => {
   const name = player;
 };
 
-console.log(gameBoard.board);
-
 function Display() {
   const container = document.querySelector(".gameboard");
+  container.innerHTML = "";
   for (let i = 0; i < 9; i++) {
     let slot = document.createElement("div");
     slot.classList.add("slot");
     slot.textContent = gameBoard.board[i];
+    slot.setAttribute("data-index", i);
+
+    slot.addEventListener("click", function (e) {
+      let index = slot.getAttribute("data-index");
+      gameBoard.UseMove(index);
+    });
+
     container.appendChild(slot);
-    console.log(gameBoard.board[i]);
   }
 }
 
